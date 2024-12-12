aa75ec751e0bc4646224c9d31ff97aa824c6a4f2
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-13 00:05:36 +0300

    Task6

---
+++ Tyuiu.RubankoGV.Sprint6.Task6.V9.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.RubankoGV.Sprint6.Task6.V9.Lib
+{
+    public class DataService : ISprint6Task6V9
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string result = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+
+
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    string[] fragments = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                    if (fragments.Length > 0)
+                    {
+                        result += fragments[^1] + " ";
+                    }
+                }
+            }
+            result = result.Trim();
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint6.Task6.V9/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.RubankoGV.Sprint6.Task6.V9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
