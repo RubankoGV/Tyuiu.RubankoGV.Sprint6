810a8159740d31dc230b1624065242e886236156
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-13 08:18:35 +0300

    Task7

---
+++ Tyuiu.RubankoGV.Sprint6.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.RubankoGV.Sprint6.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
